704b8ace65d13454a892131b892ab49574bd2be3
FrostForth <kjarboe@ucsd.edu>
2022-04-21 02:02:06 -0700

    add UI elements

--- app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Optional;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,32 @@ public class MainActivity extends AppCompatActivity {
     public void onLaunchProfileClicked(View view) {
         Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
+    }
+
+    public void onShowCounterClicked(View view) {
+        Intent intent = new Intent(this, CounterActivity.class);
+
+        TextView maxCountView = findViewById(R.id.max_count_view);
+        String maxCountStr = maxCountView.getText().toString();
+
+        Optional<Integer> maybeMaxCount = Utilities.parseCount(maxCountStr);
+
+        // Check if the integer parsed correctly.
+        if (!maybeMaxCount.isPresent()) {
+            // If not, show an error, and then return.
+            Utilities.showAlert(this, "That isn't a number!");
+            return;
+        }
+
+        // Get the integer.
+        int maxCount = maybeMaxCount.get();
+
+        // If it's not positive, show an error and then return.
+        if (maxCount <= 0) {
+            Utilities.showAlert(this, "Please enter a positive number!");
+            return;
+        }
+
+        Utilities.showAlert(this, String.format("I got the number %d", maxCount));
     }
 }
